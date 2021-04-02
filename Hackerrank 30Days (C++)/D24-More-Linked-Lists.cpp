f473d53a13e749ae2d92468514c373f89de7ecb5
Mehdi Messarat <medimessarat@gmail.com>
2021-04-02 01:53:33 +0100

    D24: More Linked Lists

---
+++ Hackerrank 30Days (C++)/D24-More-Linked-Lists.cpp
@@ -0,0 +1,94 @@
+#include <cstddef>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+class Solution
+{
+public:
+    Node *removeDuplicates(Node *head)
+    {
+        //Write your code here
+        vector<int> distinct_data;
+        Node *end = head;
+        Node *prev;
+        while (end)
+        {
+
+            int element_found = count(distinct_data.begin(), distinct_data.end(), end->data);
+            if (element_found)
+            {
+                prev->next = end->next;
+                end = prev->next;
+            }
+            else
+            {
+                distinct_data.push_back(end->data);
+                prev = end;
+                end = end->next;
+            }
+        }
+        return head;
+    }
+
+    Node *insert(Node *head, int data)
+    {
+        Node *p = new Node(data);
+        if (head == NULL)
+        {
+            head = p;
+        }
+        else if (head->next == NULL)
+        {
+            head->next = p;
+        }
+        else
+        {
+            Node *start = head;
+            while (start->next != NULL)
+            {
+                start = start->next;
+            }
+            start->next = p;
+        }
+        return head;
+    }
+    void display(Node *head)
+    {
+        Node *start = head;
+        while (start)
+        {
+            cout << start->data << " ";
+            start = start->next;
+        }
+    }
+};
+
+int main()
+{
+    Node *head = NULL;
+    Solution mylist;
+    int T, data;
+    cin >> T;
+    while (T-- > 0)
+    {
+        cin >> data;
+        head = mylist.insert(head, data);
+    }
+    head = mylist.removeDuplicates(head);
+
+    mylist.display(head);
+}
